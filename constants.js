0059370823452ac8b066afd7afd82410de7e5d48
Keith Boudreau <kboudy@gmail.com>
2020-05-16 10:07:10 -0400

    analysisResultsDir

--- constants.js
+++ constants.js
@@ -2,3 +2,4 @@ const path = require('path');
 
 exports.consumerKey = '90BBK6W8IQDOUVMB31HRFQGVTTJ15BMX';
 exports.stockDataDir = path.join(__dirname, `stockData`);
+exports.analysisResultsDir = path.join(__dirname, `analysisResults`);
