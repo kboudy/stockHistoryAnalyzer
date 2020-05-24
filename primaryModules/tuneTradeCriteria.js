2ce8dd6aa6aa9acd7b2ba99d920806201a60c80d
Keith Boudreau <kboudy@gmail.com>
2020-05-23 22:24:51 -0400

    tuneTradeCriteria_all_stocks

--- primaryModules/tuneTradeCriteria.js
+++ primaryModules/tuneTradeCriteria.js
@@ -228,13 +228,13 @@ const { argv } = require('yargs')
     max_avg_maxDownsidePercent_byBarX: [null],
     min_avg_maxUpsidePercent_byBarX: [null, 1, 2, 5],
     min_avg_profitLossPercent_atBarX: [null, 1, 2],
-    min_percentProfitable_atBarX: [null, 60, 70, 80],
-    min_percentProfitable_by_1_percent_atBarX: [null, 60, 70, 80],
+    min_percentProfitable_atBarX: [null, 60, 70, 75],
+    min_percentProfitable_by_1_percent_atBarX: [null, 60, 70],
     min_percentProfitable_by_2_percent_atBarX: [null, 60, 70],
     min_percentProfitable_by_5_percent_atBarX: [null, 60, 70],
     min_percentProfitable_by_10_percent_atBarX: [null],
     min_scoreCount: [10],
-    min_upsideDownsideRatio_byBarX: [null, 1, 1.5, 2],
+    min_upsideDownsideRatio_byBarX: [null, 1, 1.5],
   };
 
   await runBruteForceTradeSimulationAndSaveResults(
