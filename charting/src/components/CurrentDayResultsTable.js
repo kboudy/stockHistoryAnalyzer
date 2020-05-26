708e461685e2f58535cfb649955456807837b0ea
Keith Boudreau <kboudy@gmail.com>
2020-05-25 20:46:25 -0400

    colorized currentday headers

--- charting/src/components/CurrentDayResultsTable.js
+++ charting/src/components/CurrentDayResultsTable.js
@@ -7,6 +7,7 @@ import { getSignificantBars } from '../helpers/constants';
 
 import 'ag-grid-community/dist/styles/ag-grid.css';
 import 'ag-grid-community/dist/styles/ag-theme-balham.css';
+import './styles/currentDayGridStyles.css';
 
 const useStyles = makeStyles((theme) => ({}));
 
@@ -18,100 +19,124 @@ const CurrentDayResultsTable = (props) => {
     (async () => {
       const colDefs = [
         {
-          headerName: 'Criteria (past)',
+          headerName: 'Primary criteria',
+          headerClass: 'primary-header-group',
           marryChildren: true,
           children: [
             {
               headerName: 'symbol',
               field: 'symbol',
+              headerClass: 'primary-header',
             },
             {
               headerName: 'numberOfBars',
               field: 'numberOfBars',
+              headerClass: 'primary-header',
             },
             {
               headerName: 'sourceDate',
               field: 'sourceDate',
+              headerClass: 'primary-header',
             },
             {
               headerName: 'scoreCount',
               field: 'scoreCount',
+              headerClass: 'primary-header',
             },
             {
               headerName: 'avgScore',
               field: 'avgScore',
+              headerClass: 'primary-header',
             },
           ],
         },
       ];
 
+      let rotatingCssIndex = 1;
       for (const sb of await getSignificantBars()) {
         colDefs.push({
           headerName: `bar ${sb}`,
+          headerClass: `header-${rotatingCssIndex}-group`,
           marryChildren: true,
           children: [
             {
               headerName: `avg_maxUpsidePercent_byBarX.${sb}`,
               field: `avg_maxUpsidePercent_byBarX.${sb}`,
               headerTooltip: `avg_maxUpsidePercent_byBarX.${sb}`,
+              headerClass: `header-${rotatingCssIndex}`,
             },
-            {
-              headerName: `stdDev_maxUpsidePercent_byBarX.${sb}`,
-              field: `stdDev_maxUpsidePercent_byBarX.${sb}`,
-              headerTooltip: `stdDev_maxUpsidePercent_byBarX.${sb}`,
-            },
-            {
-              headerName: `avg_maxDownsidePercent_byBarX.${sb}`,
-              field: `avg_maxDownsidePercent_byBarX.${sb}`,
-              headerTooltip: `avg_maxDownsidePercent_byBarX.${sb}`,
-            },
-            {
-              headerName: `stdDev_maxDownsidePercent_byBarX.${sb}`,
-              field: `stdDev_maxDownsidePercent_byBarX.${sb}`,
-              headerTooltip: `stdDev_maxDownsidePercent_byBarX.${sb}`,
-            },
+            // {
+            //   headerName: `stdDev_maxUpsidePercent_byBarX.${sb}`,
+            //   field: `stdDev_maxUpsidePercent_byBarX.${sb}`,
+            //   headerTooltip: `stdDev_maxUpsidePercent_byBarX.${sb}`,
+            //   headerClass: `header-${rotatingCssIndex}`,
+            // },
+            // {
+            //   headerName: `avg_maxDownsidePercent_byBarX.${sb}`,
+            //   field: `avg_maxDownsidePercent_byBarX.${sb}`,
+            //   headerTooltip: `avg_maxDownsidePercent_byBarX.${sb}`,
+            //   headerClass: `header-${rotatingCssIndex}`,
+            // },
+            // {
+            //   headerName: `stdDev_maxDownsidePercent_byBarX.${sb}`,
+            //   field: `stdDev_maxDownsidePercent_byBarX.${sb}`,
+            //   headerTooltip: `stdDev_maxDownsidePercent_byBarX.${sb}`,
+            //   headerClass: `header-${rotatingCssIndex}`,
+            // },
             {
               headerName: `upsideDownsideRatio_byBarX.${sb}`,
               field: `upsideDownsideRatio_byBarX.${sb}`,
               headerTooltip: `upsideDownsideRatio_byBarX.${sb}`,
+              headerClass: `header-${rotatingCssIndex}`,
             },
             {
               headerName: `avg_profitLossPercent_atBarX.${sb}`,
               field: `avg_profitLossPercent_atBarX.${sb}`,
               headerTooltip: `avg_profitLossPercent_atBarX.${sb}`,
+              headerClass: `header-${rotatingCssIndex}`,
             },
             {
               headerName: `percentProfitable_atBarX.${sb}`,
               field: `percentProfitable_atBarX.${sb}`,
               headerTooltip: `percentProfitable_atBarX.${sb}`,
+              headerClass: `header-${rotatingCssIndex}`,
             },
             {
               headerName: `percentProfitable_by_1_percent_atBarX.${sb}`,
               field: `percentProfitable_by_1_percent_atBarX.${sb}`,
               headerTooltip: `percentProfitable_by_1_percent_atBarX.${sb}`,
+              headerClass: `header-${rotatingCssIndex}`,
             },
             {
               headerName: `percentProfitable_by_2_percent_atBarX.${sb}`,
               field: `percentProfitable_by_2_percent_atBarX.${sb}`,
               headerTooltip: `percentProfitable_by_2_percent_atBarX.${sb}`,
+              headerClass: `header-${rotatingCssIndex}`,
             },
             {
               headerName: `percentProfitable_by_5_percent_atBarX.${sb}`,
               field: `percentProfitable_by_5_percent_atBarX.${sb}`,
               headerTooltip: `percentProfitable_by_5_percent_atBarX.${sb}`,
+              headerClass: `header-${rotatingCssIndex}`,
             },
             {
               headerName: `percentProfitable_by_10_percent_atBarX.${sb}`,
               field: `percentProfitable_by_10_percent_atBarX.${sb}`,
               headerTooltip: `percentProfitable_by_10_percent_atBarX.${sb}`,
+              headerClass: `header-${rotatingCssIndex}`,
             },
-            {
-              headerName: `stdDev_profitLossPercent_atBarX.${sb}`,
-              field: `stdDev_profitLossPercent_atBarX.${sb}`,
-              headerTooltip: `stdDev_profitLossPercent_atBarX.${sb}`,
-            },
+            // {
+            //   headerName: `stdDev_profitLossPercent_atBarX.${sb}`,
+            //   field: `stdDev_profitLossPercent_atBarX.${sb}`,
+            //   headerTooltip: `stdDev_profitLossPercent_atBarX.${sb}`,
+            //   headerClass: `header-${rotatingCssIndex}`,
+            // },
           ],
         });
+        rotatingCssIndex++;
+        if (rotatingCssIndex > 5) {
+          rotatingCssIndex = 1;
+        }
       }
 
       setColumnDefs(colDefs);
