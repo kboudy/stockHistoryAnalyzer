3507f6ed63ee1aeb4d71b3733e2420031708daff
Keith Boudreau <kboudy@gmail.com>
2020-05-28 20:22:24 -0400

    currentDayJobRunner working

--- primaryModules/currentDayJobRunner.js
+++ primaryModules/currentDayJobRunner.js
@@ -2,13 +2,13 @@
 
 // trying the simple node cluster api (for parallel runs)
 // https://www.sitepoint.com/how-to-create-a-node-js-cluster-for-speeding-up-your-apps/
-const cluster = require('cluster');
-/*   _ = require('lodash'),
+const cluster = require('cluster'),
+  _ = require('lodash'),
   { runCurrentDayJob } = require('../helpers/currentDayJobRunEngine'),
   { getAvailableSymbolNames } = require('../helpers/symbolData'),
   {
     downloadAndSaveMultipleSymbolHistory,
-  } = require('../helpers/candleDownloading'), 
+  } = require('../helpers/candleDownloading'),
   { chunkArray, sleep } = require('../helpers/commonMethods'),
   chalk = require('chalk'),
   moment = require('moment'),
@@ -16,7 +16,7 @@ const cluster = require('cluster');
   os = require('os'),
   mongoApi = require('../helpers/mongoApi'),
   constants = require('../helpers/constants');
-*/
+
 // using 3/4 the cpus - might want to up that
 const numWorkers = Math.round(require('os').cpus().length * 0.75);
 const SYMBOL_CHUNK_SIZE = 50;
@@ -28,54 +28,36 @@ const { argv } = require('yargs')
   .options(argOptions);
 
 if (cluster.isMaster) {
-  //(async () => {
-  /*     await mongoApi.connectMongoose();
-    allSymbols = await getAvailableSymbolNames();
-    allSymbols = ['ACA'];
-    symbolsLeftToProcess = allSymbols.length;
-    symbolChunks = chunkArray(allSymbols, SYMBOL_CHUNK_SIZE);
- */
-  symbolChunks = [['SPY']];
-  console.log(`starting job - spinning off ${numWorkers} processes`);
-
-  //console.log('  -- downloading latest symbol data');
-  //await downloadAndSaveMultipleSymbolHistory(allSymbols);
-
-  /*     for (let i = 0; i < numWorkers; i++) {
+  (async () => {
+    await mongoApi.connectMongoose();
+    let allSymbols = await getAvailableSymbolNames();
+    let lastLoggedPercentComplete = 0;
+    let symbolsLeftToProcess = allSymbols.length;
+    let symbolChunks = chunkArray(allSymbols, SYMBOL_CHUNK_SIZE);
+    let gatheredResults = {};
+    const fireOffNewChunk = async (symbolChunk) => {
+      const worker = cluster.fork();
+      worker.on('message', (res) => {
+        const { results, originalChunkSize } = res;
+        gatheredResults = { ...gatheredResults, ...results };
+        symbolsLeftToProcess = symbolsLeftToProcess - originalChunkSize;
+      });
+      worker.send({ runTheseSymbols: symbolChunk });
+    };
+    cluster.on('exit', async (worker, code, signal) => {
       if (symbolChunks.length > 0) {
         const symbolChunk = symbolChunks[0];
         symbolChunks = symbolChunks.slice(1);
+        await fireOffNewChunk(symbolChunk);
 
-        fireOffNewChunk(symbolChunk);
+        const percentComplete = Math.round(
+          (100 * (allSymbols.length - symbolsLeftToProcess)) / allSymbols.length
+        );
+        if (percentComplete - lastLoggedPercentComplete >= 5) {
+          lastLoggedPercentComplete = percentComplete;
+          console.log(`${percentComplete}%`);
+        }
       }
-    } */
-  cluster.on('exit', async (worker, code, signal) => {
-    /*       console.log(
-        'Worker ' +
-          worker.process.pid +
-          ' died with code: ' +
-          code +
-          ', and signal: ' +
-          signal
-      ); */
-    if (symbolChunks.length > 0) {
-      const symbolChunk = symbolChunks[0];
-      symbolChunks = symbolChunks.slice(1);
-
-      const worker = cluster.fork();
-      worker.on('message', (res) => {
-        console.log('hi');
-        /*       const { results, originalChunkSize } = res;
-      gatheredResults = { ...gatheredResults, ...results };
-      symbolsLeftToProcess = symbolsLeftToProcess - originalChunkSize;
-      const percentComplete = Math.round(
-        (100 * (allSymbols.length - symbolsLeftToProcess)) / allSymbols.length
-      );
-      if (percentComplete - lastLoggedPercentComplete >= 5) {
-        lastLoggedPercentComplete = percentComplete;
-        console.log(`${percentComplete}%`);
-      }
-      console.log(`symbolsLeftToProcess: ${symbolsLeftToProcess}`);
       if (symbolsLeftToProcess === 0) {
         // done - save the gathered results
         await CurrentDayEvaluationJobRun.create({
@@ -84,24 +66,32 @@ if (cluster.isMaster) {
         });
         await mongoApi.disconnectMongoose();
         console.log('job complete');
-      } */
-      });
-      worker.send({ runTheseSymbols: symbolChunk });
+      }
+    });
+    console.log(`starting job - spinning off ${numWorkers} processes`);
+
+    console.log('  -- downloading latest symbol data');
+    await downloadAndSaveMultipleSymbolHistory(allSymbols);
+
+    for (let i = 0; i < numWorkers; i++) {
+      if (symbolChunks.length > 0) {
+        const symbolChunk = symbolChunks[0];
+        symbolChunks = symbolChunks.slice(1);
+
+        await fireOffNewChunk(symbolChunk);
+      }
     }
-  });
-  // })();
+  })();
 } else {
   process.on('message', (message) => {
-    process.send('message received');
-    /*     (async () => {
+    (async () => {
       await mongoApi.connectMongoose();
-      console.log('message from the master');
       //message from the master
       const { runTheseSymbols } = message;
       const results = await runCurrentDayJob(runTheseSymbols);
       process.send({ results, originalChunkSize: runTheseSymbols.length });
       await mongoApi.disconnectMongoose();
       process.exit(0);
-    })(); */
+    })();
   });
 }
