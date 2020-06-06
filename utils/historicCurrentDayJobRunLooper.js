e37daee588045c0e6e70936e553b825ac798c542
Keith Boudreau <kboudy@gmail.com>
2020-06-06 18:57:23 -0400

    looping back in history for currentDayJob

--- utils/historicCurrentDayJobRunLooper.js
+++ utils/historicCurrentDayJobRunLooper.js
@@ -1,19 +1,39 @@
 // purpose: run the currentDayJob on historical dates.  apply the stringent filter & create paper trades
-const util = require('util'),
+const mongoApi = require('../helpers/mongoApi'),
+  util = require('util'),
   path = require('path'),
+  moment = require('moment'),
+  Candle = require('../models/candle'),
+  CurrentDayEvaluationJobRun = require('../models/currentDayEvaluationJobRun'),
   exec = util.promisify(require('child_process').exec);
 
 (async () => {
-  const jobRunnerPath = `${path.join(
-    path.dirname(__dirname),
-    'primaryModules/currentDayJobRunner.js'
-  )}`;
+  await mongoApi.connectMongoose();
 
-  try {
-    const { stdOut, stdErr } = await exec(
-      `node "${jobRunnerPath}" --historicalDate 2020-06-03`
-    );
-  } catch (err) {
-    console.log(err);
+  const allDates = (await Candle.find({ symbol: 'SPY' })).map((c) => c.date);
+  const earliestJobDate = moment(
+    (await CurrentDayEvaluationJobRun.findOne({}).sort({ created: 1 }).limit(1))
+      .created
+  ).format('YYYY-MM-DD');
+  let currentLoopDate = allDates.filter((d) => d < earliestJobDate);
+  currentLoopDate = currentLoopDate[currentLoopDate.length - 1];
+
+  for (let i = 0; i < 50; i++) {
+    const jobRunnerPath = `${path.join(
+      path.dirname(__dirname),
+      'primaryModules/currentDayJobRunner.js'
+    )}`;
+
+    try {
+      const { stdOut, stdErr } = await exec(
+        `node "${jobRunnerPath}" --historicalDate ${currentLoopDate}`
+      );
+    } catch (err) {
+      console.log(err);
+    }
+    currentLoopDate = allDates.filter((d) => d < currentLoopDate);
+    currentLoopDate = currentLoopDate[currentLoopDate.length - 1];
   }
+
+  await mongoApi.connectMongoose();
 })();
